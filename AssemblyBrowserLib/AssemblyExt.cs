63ec6be55b82f49b2ffa389e6f25d5824fd58c6f
ZaharPro <zahar7331@gmail.com>
2021-09-27 16:25:24 +0300

    Added printing of type and method definitions

---
+++ AssemblyBrowserLib/AssemblyExt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace AssemblyBrowserLib
+{
+    public static class AssemblyExt
+    {
+        public static IDictionary<string, IEnumerable<Type>> Namespaces(this Assembly assembly)
+        {
+            _ = assembly ?? throw new ArgumentNullException(nameof(assembly));
+            Dictionary<string, List<Type>> namespaces = new();
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (!namespaces.TryGetValue(type.Namespace, out List<Type> list))
+                {
+                    list = new List<Type>();
+                    namespaces.Add(type.Namespace, list);
+                }
+                list.Add(type);
+            }
+            return (IDictionary<string, IEnumerable<Type>>)namespaces;
+        }
+    }
+}
---
+++ AssemblyBrowserLib/TypeExt.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AssemblyBrowserLib
+{
+    public static class TypeExt
+    {
+        public static string PrintVisibility(this Type type)
+        {
+            if (type.IsPublic || type.IsNestedPublic)
+                return "public";
+            if (type.IsNestedPrivate)
+                return "private";
+            if (type.IsNestedFamily)
+                return "protected";
+            if (type.IsNestedAssembly)
+                return "internal";
+            if (type.IsNestedFamORAssem)
+                return "protected internal";
+            if (type.IsNestedFamANDAssem)
+                return "private internal";
+            return "private";
+        }
+        public static string PrintTypeOfClassExtension(this Type type)
+        {
+            if (type.IsAbstract)
+                return type.IsSealed ? "static" : "abstract";
+            if (type.IsSealed)
+                return "sealed";
+            return "";
+        }
+        public static string PrintSemantics(this Type type)
+        {
+            return type.IsInterface ? "interface" :
+                type.IsValueType ? "struct" : "class";
+        }
+
+        public static string PrintDefinitionName(this Type type)
+        {
+            if (type.IsGenericType)
+            {
+                Type[] genericArgs = type.GetGenericArguments();
+                StringBuilder sb = new(type.Name);
+                sb.Length--;
+                sb.Length -= genericArgs.Length.ToString().Length;
+                sb.Append('<');
+                if (genericArgs.Length != 0)
+                {
+                    foreach (Type t in genericArgs)
+                    {
+                        sb.Append(t.PrintDefinitionName())
+                            .Append(',');
+                    }
+                    sb.Length--;
+                }
+                sb.Append('>');
+                return sb.ToString();
+            }
+            else
+            {
+                return type.Name;
+            }
+        }
+
+        public static string PrintDeclaration(this Type type)
+        {
+            var strings = new string[] {
+                type.PrintVisibility(),
+                type.PrintTypeOfClassExtension(),
+                type.PrintSemantics(),
+                type.PrintDefinitionName()
+            }.Where(s => !string.IsNullOrEmpty(s));
+
+            string declaration = string.Join(' ', strings);
+            List<string> list = new();
+            list.Add(type.BaseType?.PrintDefinitionName());
+            list.AddRange(type.GetInterfaces().Select(i => i.PrintDefinitionName()));
+            list.RemoveAll(s => string.IsNullOrEmpty(s));
+            if (list.Count != 0)
+            {
+                declaration += ": " + string.Join(',', list);
+            }
+            return declaration;
+        }
+    }
+}
